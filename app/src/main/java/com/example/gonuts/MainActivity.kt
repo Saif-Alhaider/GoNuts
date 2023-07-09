8c23f23d1e128afb94c608e9d71525638283ca56
Saif Alhaider <saifharith90@gmail.com>
2023-07-09 04:01:49 +0300

    edit:make status bar transparent

--- app/src/main/java/com/example/gonuts/MainActivity.kt
+++ app/src/main/java/com/example/gonuts/MainActivity.kt
@@ -3,18 +3,22 @@ package com.example.gonuts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.gonuts.ui.theme.GoNutsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             GoNutsTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +26,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(Modifier.systemBarsPadding()) {
+                        Greeting("Android")
+                    }
                 }
             }
         }
