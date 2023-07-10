cff4bc9501d69c2d4e74a157b2084afa7b16b999
Saif Alhaider <saifharith90@gmail.com>
2023-07-10 11:10:24 +0300

    add: HomeViewModel

---
+++ app/src/main/java/com/example/gonuts/ui/viewmodel/home/HomeViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.gonuts.ui.viewmodel.home
+
+import androidx.lifecycle.ViewModel
+import com.example.gonuts.usecase.GetDoughnutsUseCase
+import com.example.gonuts.usecase.GetTodayOffersUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getTodayOffersUseCase: GetTodayOffersUseCase,
+    private val getDoughnutsUseCase: GetDoughnutsUseCase,
+) : ViewModel() {
+    private val _state = MutableStateFlow(HomeUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        getTodayOffers()
+        getDoughnuts()
+    }
+
+    private fun getTodayOffers() {
+        val todayOffers = getTodayOffersUseCase().map { it.mapToUi() }
+        _state.update { it.copy(todayOffers = todayOffers) }
+    }
+
+    private fun getDoughnuts() {
+        val doughnuts = getDoughnutsUseCase().map { it.mapToUi() }
+        _state.update { it.copy(doughnuts = doughnuts) }
+    }
+}
