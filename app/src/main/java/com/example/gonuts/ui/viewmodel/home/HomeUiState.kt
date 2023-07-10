9a1f51bff8060a9f3287407d49f86ca189f295e8
Saif Alhaider <saifharith90@gmail.com>
2023-07-10 10:57:59 +0300

    add:home ui state

---
+++ app/src/main/java/com/example/gonuts/ui/viewmodel/home/HomeUiState.kt
@@ -0,0 +1,19 @@
+package com.example.gonuts.ui.viewmodel.home
+
+import com.example.gonuts.entity.Doughnut
+import com.example.gonuts.ui.viewmodel.common.state.DoughnutUiState
+
+data class HomeUiState(
+    val todayOffers: List<DoughnutUiState> = emptyList(),
+    val doughnuts: List<DoughnutUiState> = emptyList()
+)
+
+fun Doughnut.mapToUi(): DoughnutUiState {
+    return DoughnutUiState(
+        name = this.name,
+        description = this.description,
+        originalPrice = this.originalPrice,
+        discountedPrice = this.discountedPrice,
+        imageResource = this.imageResource
+    )
+}
