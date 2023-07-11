eba4c1bbfd090ae00a06b8e91559588a43b3ba3e
Saif Alhaider <saifharith90@gmail.com>
2023-07-11 05:54:15 +0300

    add: DoughnutDetailsViewModel

--- app/src/main/java/com/example/gonuts/ui/screen/doughnut_details/DoughnutDetailsViewModel.kt
+++ app/src/main/java/com/example/gonuts/ui/screen/doughnut_details/DoughnutDetailsViewModel.kt
@@ -1,14 +1,42 @@
 package com.example.gonuts.ui.screen.doughnut_details
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.example.gonuts.ui.common.state.DoughnutUiState
+import com.example.gonuts.ui.screen.home.AmountSelectorListener
+import com.example.gonuts.ui.screen.home.mapToUi
+import com.example.gonuts.usecase.GetDoughnutByName
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class DoughnutDetailsViewModel @Inject constructor(): ViewModel() {
-    private val _state = MutableStateFlow(DoughnutUiState())
+class DoughnutDetailsViewModel @Inject constructor(
+    private val getDoughnutByName: GetDoughnutByName,
+    savedStateHandle: SavedStateHandle
+) :
+    ViewModel(), AmountSelectorListener {
+    private val _state = MutableStateFlow(DoughnutDetailsUiState())
     val state = _state.asStateFlow()
+
+    private val args: DoughnutDetailsArgs = DoughnutDetailsArgs(savedStateHandle)
+
+    init {
+        getDoughnut()
+    }
+
+    private fun getDoughnut() {
+        val doughnut = getDoughnutByName().first { it.name == args.name }
+        _state.update { it.copy(doughnut = doughnut.mapToUi()) }
+    }
+
+    override fun onClickIncrement() {
+        _state.update { it.copy(amount = it.amount + 1) }
+    }
+
+    override fun onClickDecrement() {
+        _state.update { it.copy(amount = if (it.amount == 0) 0 else it.amount - 1) }
+    }
 }
