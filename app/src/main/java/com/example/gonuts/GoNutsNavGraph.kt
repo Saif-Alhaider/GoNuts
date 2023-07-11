ef03db186a1a3571c4bd1a1cd8b1109ebefa03e1
Saif Alhaider <saifharith90@gmail.com>
2023-07-11 05:00:12 +0300

    add: nav graph

--- app/src/main/java/com/example/gonuts/GoNutsNavGraph.kt
+++ app/src/main/java/com/example/gonuts/GoNutsNavGraph.kt
@@ -3,16 +3,15 @@ package com.example.gonuts
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import com.example.gonuts.ui.screen.doughnut_details.doughnutDetailsRoute
 import com.example.gonuts.ui.screen.home.homeRoute
-import com.example.gonuts.ui.screen.welcome.WelcomeContent
-import com.example.gonuts.ui.screen.welcome.WelcomeScreen
 import com.example.gonuts.ui.screen.welcome.welcomeRoute
 
 @Composable
 fun GoNutsNavGraph(navHostController: NavHostController) {
-    NavHost(navController = navHostController, startDestination = Screen.Welcome.route) {
+    NavHost(navController = navHostController, startDestination = AppDestination.Welcome.route) {
         welcomeRoute(navHostController)
         homeRoute(navHostController)
+        doughnutDetailsRoute(navHostController)
     }
 }
