5c82b469d768c389db1fde4e9ace5d5710ede71b
Saif Alhaider <saifharith90@gmail.com>
2023-07-11 09:06:09 +0300

    add: onclick add to favourite make it filled icon

--- app/src/main/java/com/example/gonuts/ui/common/composables/AddToFavouriteIcon.kt
+++ app/src/main/java/com/example/gonuts/ui/common/composables/AddToFavouriteIcon.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
@@ -16,12 +18,13 @@ import com.example.gonuts.ui.theme.GoNutsCustomColors
 
 @Composable
 fun AddToFavouriteIcon(modifier: Modifier = Modifier, shape: Shape = CircleShape) {
+    val isActive = remember { mutableStateOf(false) }
     Image(
-        painter = painterResource(id = R.drawable.heart_add_to_favourite),
+        painter = painterResource(id = if (isActive.value) R.drawable.ic_heart_filled else R.drawable.heart_add_to_favourite),
         contentDescription = "add to favourite icon",
         modifier = modifier
             .clip(shape)
-            .clickable { }
+            .clickable { isActive.value = !isActive.value }
             .background(GoNutsCustomColors.current.background)
             .padding(12.dp)
     )
