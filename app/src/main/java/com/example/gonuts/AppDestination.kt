6e3ef90b96fa889aa944d94ec62419aa56be2b45
Saif Alhaider <saifharith90@gmail.com>
2023-07-11 09:06:39 +0300

    add: bottom navigation destinations

--- app/src/main/java/com/example/gonuts/AppDestination.kt
+++ app/src/main/java/com/example/gonuts/AppDestination.kt
@@ -1,7 +1,13 @@
 package com.example.gonuts
 
-sealed class AppDestination(val route: String) {
-    object Welcome : AppDestination(route = "welcome")
-    object Home : AppDestination(route = "home")
-    object DoughnutDetails:AppDestination(route = "doughnut_details")
+import androidx.annotation.DrawableRes
+
+sealed class AppDestination(val route: String, @DrawableRes val icon: Int? = null) {
+    object Welcome : AppDestination("welcome")
+    object Home : AppDestination("home",icon = R.drawable.ic_home)
+    object DoughnutDetails : AppDestination("doughnut_details")
+    object Favourite : AppDestination("favourite",R.drawable.ic_heart_bottom_navigation)
+    object Notification : AppDestination("notification",R.drawable.ic_notification)
+    object Cart : AppDestination("cart",R.drawable.ic_cart)
+    object Profile : AppDestination("profile",R.drawable.ic_profile)
 }
