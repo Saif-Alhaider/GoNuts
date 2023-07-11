9ade1c9e31b0f5f0f66d4fa6aefe74eb86c79e0e
Saif Alhaider <saifharith90@gmail.com>
2023-07-11 03:32:59 +0300

    add: GoNutsNavGraph

---
+++ app/src/main/java/com/example/gonuts/GoNutsNavGraph.kt
@@ -0,0 +1,18 @@
+package com.example.gonuts
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.gonuts.ui.screen.home.homeRoute
+import com.example.gonuts.ui.screen.welcome.WelcomeContent
+import com.example.gonuts.ui.screen.welcome.WelcomeScreen
+import com.example.gonuts.ui.screen.welcome.welcomeRoute
+
+@Composable
+fun GoNutsNavGraph(navHostController: NavHostController) {
+    NavHost(navController = navHostController, startDestination = "welcome") {
+        welcomeRoute(navHostController)
+        homeRoute(navHostController)
+    }
+}
