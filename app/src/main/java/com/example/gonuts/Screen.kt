b32eae7e5d519541234a1bc1ceaf272e5121eb95
Saif Alhaider <saifharith90@gmail.com>
2023-07-11 04:59:44 +0300

    rename: Screen.kt

--- app/src/main/java/com/example/gonuts/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gonuts
-
-sealed class Screen(val route: String) {
-    object Welcome : Screen(route = "welcome")
-    object Home : Screen(route = "home")
-    object DoughnutDetails:Screen(route = "doughnut_details")
-}
