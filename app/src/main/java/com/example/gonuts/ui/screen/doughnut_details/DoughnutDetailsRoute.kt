62e3b25c183e35cec0c7ef6ee642a4d747a7bc6e
Saif Alhaider <saifharith90@gmail.com>
2023-07-11 05:53:27 +0300

    add: DoughnutDetailsRoute

---
+++ app/src/main/java/com/example/gonuts/ui/screen/doughnut_details/DoughnutDetailsRoute.kt
@@ -0,0 +1,33 @@
+package com.example.gonuts.ui.screen.doughnut_details
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+private const val ROUTE = "doughnut_details"
+
+fun NavController.navigateToDoughnutDetails(name:String) {
+    navigate("$ROUTE/${name}")
+}
+
+fun NavGraphBuilder.doughnutDetailsRoute(navController: NavController) {
+    composable(
+        "$ROUTE/{${DoughnutDetailsArgs.NAME_ARG}}",
+        arguments = listOf(navArgument("name") { NavType.StringType })
+    ) {
+        DoughnutDetailsScreen(
+            navController
+        )
+    }
+}
+
+class DoughnutDetailsArgs(savedStateHandle: SavedStateHandle) {
+    val name: String = checkNotNull(savedStateHandle[NAME_ARG])
+
+    companion object {
+        const val NAME_ARG = "name"
+    }
+}
